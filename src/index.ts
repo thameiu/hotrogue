8174375025b158e1e791a12e60e0a783e6cb98a2
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2024-12-08 10:09:38 +0100

    modified vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Bienvenue dans le projet Node.js avec TypeScript !");
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/user';
+import authRoutes from './routes/auth';
+import gameRoutes from './routes/game';
+import itemRoutes from './routes/item';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
+app.use('/game', gameRoutes);
+app.use('/item', itemRoutes);
+
+app.get("/", (req, res) => res.send("HOTROGUE !!!!") as any);
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
