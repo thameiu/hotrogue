fafbe179bf290183dacf5364b4006e25e9adbe77
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2024-12-09 17:14:47 +0100

    removing listen test

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,8 @@ app.get("/", (req, res) => res.send("HOTROGUE !!!!") as any);
 
 app.get("/home", (req, res) => res.send("bonjour") as any);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 
 export default app;
