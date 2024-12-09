444244ae63bec1a96dff5ef130ba7f8ff3990a9b
Mathieu HERNANDEZ <hernandez.mathieu19@gmail.com>
2024-12-09 16:18:50 +0100

    modified vercel stuff

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import userRoutes from './routes/user';
-import authRoutes from './routes/auth';
-import gameRoutes from './routes/game';
-import itemRoutes from './routes/item';
+import app from './app';
 
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use('/users', userRoutes);
-app.use('/auth', authRoutes);
-app.use('/game', gameRoutes);
-app.use('/item', itemRoutes);
-
-app.get("/", (req, res) => res.send("HOTROGUE !!!!") as any);
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-module.exports = app;
+export default app;
